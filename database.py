7bbfd53df5b73f70ee90afaee217d2d564504ec1
Shikazu777 <dhanrax@gmail.com>
2026-01-16 13:45:39 +0530

    added canteen order

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:password@localhost/campusdb"
+DATABASE_URL = "postgresql://postgres:admin123@localhost:5432/campusdb"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
--- models.py
+++ models.py
@@ -1,10 +1,9 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime
-from database import Base
+from sqlalchemy import Column, Integer, Float, String, DateTime
 from datetime import datetime
+from database import Base
 
 class Student(Base):
     __tablename__ = "students"
-
     id = Column(Integer, primary_key=True)
     email = Column(String, unique=True)
     trust_score = Column(Float, default=50.0)
@@ -13,10 +12,20 @@ class Student(Base):
 
 class Transaction(Base):
     __tablename__ = "transactions"
-
     id = Column(Integer, primary_key=True)
     student_id = Column(Integer)
     amount = Column(Float)
-    category = Column(String)  # canteen / event
-    status = Column(String)    # success / failed
+    category = Column(String)
+    status = Column(String)
     timestamp = Column(DateTime, default=datetime.utcnow)
+
+
+class CanteenOrder(Base):
+    __tablename__ = "canteen_orders"
+    id = Column(Integer, primary_key=True)
+    student_id = Column(Integer)
+    item = Column(String)
+    total_amount = Column(Float)
+    advance_paid = Column(Float)
+    status = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
